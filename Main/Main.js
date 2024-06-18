c26a4030d1acb12287693f54a20e2ee542debf59
matiasrevetria <revetriamatias03@gmail.com>
2024-06-17 23:47:28 -0300

    2.0

--- Main/Main.js
+++ Main/Main.js
@@ -8,3 +8,18 @@ function saludo(callback) {
 };
 
 saludo(saludar);
+
+function Animal(nombre,especie){
+    this.nombre = nombre;
+    this.especie = especie;
+}
+
+Animal.prototype = {
+    hablar: ()=>{
+        console.log("Hola soy "+this.nombre+" y soy un "+this.especie);
+    }
+}
+
+const Dog = Object.create(Animal.prototype);
+
+
